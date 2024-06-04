fd8edf25892dfd1a46ea3723cfdf6e93652d5ce7
mohsen <107324736+mohsen81sa@users.noreply.github.com>
2024-06-04 16:33:40 +0330

    admin,profile,signal
    admin
    profile
    signal

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import(
     BaseUserManager,AbstractBaseUser,PermissionsMixin
 )
 from django.utils.translation import ugettext_lazy as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 class UserManager(BaseUserManager):
     """
@@ -42,7 +44,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     """
     Custom User Model for authentication management through email address instead of username
     """
-
     email = models.EmailField(max_length=255, unique=True)
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
@@ -59,3 +60,28 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+    
+class Profile(models.Model):
+    """
+    Profile class for each user which is being created to hold the information
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    image = models.ImageField(blank=True, null=True)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    """
+    Signal for post creating a user which activates when a user being created ONLY
+    """
+    if created:
+        Profile.objects.create(user=instance)
