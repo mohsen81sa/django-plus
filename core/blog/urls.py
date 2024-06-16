4c10a186e94612678ed030852da32508be6d42a4
mohsen <107324736+mohsen81sa@users.noreply.github.com>
2024-06-16 20:39:08 +0330

    templateView

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html",extra_context={"name":"mohsen"})),
+    # path("", TemplateView.as_view(template_name="index.html",extra_context={"name":"mohsen"})),
+    path("1", views.Indexview.as_view(),name='1')
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,5 +1,16 @@
+from typing import Any
 from django.shortcuts import render
-
+from django.views.generic.base import TemplateView
+from .models import Post
 # Create your views here.
 def IndexView(request):
     return render(request,"index.html")
+
+class Indexview(TemplateView):
+    template_name='index.html'
+
+    def get_context_data(self, **kwargs):
+        context= super().get_context_data(**kwargs)
+        context["name"] = "mohsen"
+        context["posts"] = Post.objects.all()
+        return context
