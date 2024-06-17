4231eaa31134517fa78325edfed2d102f80ae2fe
mohsen <107324736+mohsen81sa@users.noreply.github.com>
2024-06-17 13:19:00 +0330

    RidirectView
    Ridirect

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,RedirectView
 from . import views
 app_name = "blog"
 
 urlpatterns = [
     # path("", TemplateView.as_view(template_name="index.html",extra_context={"name":"mohsen"})),
-    path("1", views.Indexview.as_view(),name='1')
+    path("blog-index", views.Indexview.as_view(),name='index'),
+    path("go-to-index",RedirectView.as_view(pattern_name="blog:index"),name="go-to-index")
 ]
